e3fa74bc5f2fe9419d348bcd2115829d758b7233
pawlu87 <proton87@gmail.com>
2020-01-21 23:23:40 -0500

    bugfixes

--- run.sh
+++ run.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-if [ $(command -v ansible) != "/usr/bin/ansible" ]; then
+if [[ $(command -v ansible) == "" ]]; then
+	echo "installing ansible"
 	sudo apt-get install ansible
 fi
 
-if [[ -f "~/.ssh/id_rsa" ]]; then
+if [[ $(command -v openssh-server) == "" ]]; then
+	echo "installing openssh-server"
+	sudo apt-get install openssh-server
+fi
+
+if [[ ! -f ~/.ssh/id_rsa ]]; then
+	echo "- adding id_rsa"
 	ssh-keygen
 fi
 
-if [[ -f "~/.ssh/authorized_keys" ]]; then
+if [[ ! -f ~/.ssh/authorized_keys ]]; then
+	echo "- adding authorized key"
 	ssh-copy-id $(whoami)@localhost
 fi
 
